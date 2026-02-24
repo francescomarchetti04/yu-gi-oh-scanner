2e713039b63c17736b9d2d2f3718ab17cf4dfe4a
Francesco Marchetti <francymarchtti04@gmail.com>
2026-02-24 17:11:21 +0100

    scrittura network pckg

--- YGOscanner/stub/AudioRecord.java
+++
@@ -1,9 +0,0 @@
-package YGOscanner.stub;
-
-public class AudioRecord {
-    public AudioRecord(int source, int sampleRate, int channelConfig, int audioFormat, int bufferSize) {}
-
-    public void startRecording() {}
-    public void stop() {}
-    public int read(short[] buffer, int offset, int size) { return 0; }
-}
